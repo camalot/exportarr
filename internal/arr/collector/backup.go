b0e64a46fc70f7069fc0457c79db667df6ef3044
Ryan Conrad <camalot@gmail.com>
2024-11-13 13:14:09 -0600

    added backup metrics

--- internal/arr/collector/backup.go
+++ internal/arr/collector/backup.go
@@ -1,6 +1,9 @@
 package collector
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/onedr0p/exportarr/internal/arr/client"
 	"github.com/onedr0p/exportarr/internal/arr/config"
 	"github.com/onedr0p/exportarr/internal/arr/model"
@@ -9,18 +12,26 @@ import (
 )
 
 type backupCollector struct {
-	config        *config.ArrConfig // App configuration
-	backupMetric *prometheus.Desc  // Total number of root folders
-	errorMetric   *prometheus.Desc  // Error Description for use with InvalidMetric
+	config           *config.ArrConfig // App configuration
+	backupsMetric    *prometheus.Desc  // 
+	lastBackupMetric *prometheus.Desc  // 
+	errorMetric      *prometheus.Desc  // Error Description for use with InvalidMetric
 }
 
 func NewBackupCollector(conf *config.ArrConfig) *backupCollector {
 	return &backupCollector{
 		config: conf,
-		backupMetric: prometheus.NewDesc(
+		backupsMetric: prometheus.NewDesc(
 			prometheus.BuildFQName(conf.App, "system", "backup_bytes"),
 			"Backup metrics for the application.",
-			[]string{"name", "type", "time"},
+			[]string{"name", "type", "time", "age"},
+			prometheus.Labels{"url": conf.URL},
+		),
+
+		lastBackupMetric: prometheus.NewDesc(
+			prometheus.BuildFQName(conf.App, "system", "backup"),
+			"The time in seconds since the last backup.",
+			[]string{"name", "type"},
 			prometheus.Labels{"url": conf.URL},
 		),
 
@@ -34,7 +45,8 @@ func NewBackupCollector(conf *config.ArrConfig) *backupCollector {
 }
 
 func (collector *backupCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- collector.backupMetric
+	ch <- collector.backupsMetric
+	ch <- collector.lastBackupMetric
 }
 
 func (collector *backupCollector) Collect(ch chan<- prometheus.Metric) {
@@ -55,14 +67,50 @@ func (collector *backupCollector) Collect(ch chan<- prometheus.Metric) {
 	}
 
 	if len(backups) > 0 {
+		// store the last backup time
+		type lastBackup struct {
+			age      int64
+			name     string
+			typeName string
+		}
+		var lastBackupInfo lastBackup = lastBackup{age: -1, name: "", typeName: ""}
+		const layout = "2024-11-05T16:48:59Z"
 		for _, backup := range backups {
+			// convert the date time string to a unix timestamp
+			backupTime, err := time.Parse(layout, backup.Time)
+			if err != nil {
+				log.Errorw("Error parsing backup time",
+					"error", err)
+				ch <- prometheus.NewInvalidMetric(collector.errorMetric, err)
+			}
+			backupUnix := backupTime.Unix()
+			unixNow := time.Now().Unix()
+			age := unixNow - backupUnix
+			if age < lastBackupInfo.age || lastBackupInfo.age == -1 {
+				lastBackupInfo.age = age
+				lastBackupInfo.name = backup.Name
+				lastBackupInfo.typeName = backup.Type
+			}
+
 			ch <- prometheus.MustNewConstMetric(
-				collector.backupMetric,
+				collector.backupsMetric,
 				prometheus.GaugeValue,
 				float64(backup.Size),
 				backup.Name,
 				backup.Type,
 				backup.Time,
+				strconv.FormatInt(age, 10),
+			)
+		}
+
+		if lastBackupInfo.age != -1 {
+			// store the last backup time
+			ch <- prometheus.MustNewConstMetric(
+				collector.lastBackupMetric,
+				prometheus.GaugeValue,
+				float64(lastBackupInfo.age),
+				lastBackupInfo.name,
+				lastBackupInfo.typeName,
 			)
 		}
 	}
